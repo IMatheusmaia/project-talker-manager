09c40b35a61b2b742dc5824c89f9f549cb42f3cc
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-02-07 05:09:41 -0300

    requisito 2

--- src/index.js
+++ src/index.js
@@ -31,3 +31,18 @@ app.get('/talker', async (_req, res) => {
     res.status(500).json({ message: 'Erro ao ler o arquivo' });
   }
 });
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await fs.readFile(path.resolve(__dirname, DATA_PATH), 'utf-8');
+    const data = JSON.parse(response);
+    const found = data.find((item) => item.id === Number(id));
+    if (typeof found === 'undefined') {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(found);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao ler o arquivo' });
+  }
+});
