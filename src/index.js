a921556e7eb22c8e9631a3968e9e735a482220b0
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-02-07 04:31:41 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
@@ -12,5 +14,20 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log(`Online e rodando na porta ${PORT}!`);
+});
+
+const DATA_PATH = './talker.json';
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const response = await fs.readFile(path.resolve(__dirname, DATA_PATH), 'utf-8');
+    const talkers = JSON.parse(response);
+    if (talkers.length === 0) {
+      return res.status(200).json([]);
+    }
+    return res.status(200).json(talkers);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao ler o arquivo' });
+  }
 });
